b21122d48d933c79aed0357d1cb279d53ad6af0d
chuxin <15115359055@139.com>
2024-07-28 20:09:30 +0800

    feat: 用于对fs的学习~

--- demo-fs/main.js
+++ demo-fs/main.js
@@ -3,14 +3,21 @@ const fs = require("fs")
 // 创建文件夹
 fs.mkdir("./dist", (err) => {
     // EEXIST 代表已经存在了
-    if(err && err.code !== "EEXIST"){
+    if (err && err.code == "EEXIST") {
         console.log("目录已存在")
         return;
     }
     console.log("创建成功")
 })
 
-//
+// 修改名字
+fs.rename("./dist", "./dist1", (err) => {
+    if (err && err.code == "ENOENT") {
+        console.log("修改失败")
+        return;
+    }
+    console.log("修改成功~")
+})
 
 
 
