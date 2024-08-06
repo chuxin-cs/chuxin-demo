ebc0884009dd2abea3f4b0cf802b61a4a415a557
chuxin <15115359055@139.com>
2024-08-06 14:43:36 +0800

    feat:  fs.readJsonSync 和 fs.writeJsonSync 完成对 json文件的读取与修改~

--- demo-fs-extra/src/main.js
+++ demo-fs-extra/src/main.js
@@ -5,20 +5,25 @@ import { fileURLToPath } from "node:url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 //1.创建文件
-fs.ensureFileSync(path.resolve(__dirname, "a.vue"));
+// fs.ensureFileSync(path.resolve(__dirname, "a.vue"));
+
 //2.删除文件
 // fs.removeSync(path.resolve(__dirname, "a.vue"));
+
 //3.删除文件夹
 // fs.removeSync(path.resolve(__dirname, "ee"));
+
 //4.创建文件夹
 // fs.ensureDirSync(path.resolve(__dirname, "c/c"));
 // fs.mkdirsSync(path.resolve(__dirname, "d/d"));
 // fs.mkdirpSync(path.resolve(__dirname, "ee/ee"));
+
 //5.修改文件夹
 // fs.renameSync(
 //   path.resolve(__dirname, "a.vue"),
 //   path.resolve(__dirname, "b.vue")
 // );
+
 //6.读取 json 文件
 // let aJson = fs.readJsonSync(path.resolve(__dirname, "a.json"));
 // aJson.author = "chuXin";
