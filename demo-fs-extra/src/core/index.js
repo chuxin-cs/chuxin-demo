9e52a977cd80ee3a258e3c0096ad815d371dc42c
chuxin <15115359055@139.com>
2024-08-06 13:51:15 +0800

    feat: 用于 fs-extra 学习

---
+++ demo-fs-extra/src/core/index.js
@@ -0,0 +1,9 @@
+import fs from "fs-extra";
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export const author = "chuXin";
+
+console.log(__dirname, 1);
---
+++ demo-fs-extra/src/main.js
@@ -0,0 +1,10 @@
+import { author } from "./core/index.js";
+
+console.log(author);
+
+//1、增
+//2、删
+//3、改
+//4、查
+//5、拷贝
+//6、判断是否是文件 或者是文件夹
