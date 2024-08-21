b2dbbe623518d62285419ae133866583542b1e9b
chuxin <15115359055@139.com>
2024-08-21 15:48:09 +0800

    feat: add zustand useStore

--- react/demo-zustand/src/App.tsx
+++ react/demo-zustand/src/App.tsx
@@ -1,4 +1,5 @@
 import { useUserStore } from "@/store";
+import { useStore } from "./store/name";
 function App() {
   // 调用这个函数直接可以获取
   let name = useUserStore((state: any) => state.name);
@@ -16,9 +17,19 @@ function App() {
   const userStore = useUserStore;
   console.log(userStore);
 
+  const _name = useStore((state) => state.name);
+  const _setName = useStore((state) => state.setName);
+  const _removeName = useStore((state) => state.removeName);
+  const _updateName = useStore((state) => state.updateName);
+
   return (
     <>
       <h1>zustand</h1>
+      <div>{_name}</div>
+      <button onClick={_setName}>_setName</button>
+      <button onClick={_removeName}>_removeName</button>
+      <button onClick={() => _updateName("luanshu")}>_updateName</button>
+
       <div>{name}</div>
       <button onClick={() => updateName(1)}>直接修改</button>
       <button onClick={() => handleName(111)}>修改name</button>
