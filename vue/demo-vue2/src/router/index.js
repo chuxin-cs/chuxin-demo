753cb81a72e022f12fe438947c9260cfc11e3f6e
chuxin <15115359055@139.com>
2024-08-21 10:25:35 +0800

    feat: 收纳 vue2 相关demo

---
+++ vue/demo-vue2/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/home/index.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/about/index.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ vue/demo-vue2/src/views/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+
+    <div>
+      <el-input
+        v-model="input"
+        v-valid-forbid
+        placeholder="请输入内容"
+      ></el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      input: "",
+    };
+  },
+};
+</script>
