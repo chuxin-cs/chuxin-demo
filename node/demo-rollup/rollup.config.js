2a64e760604b5eab3b2ff3d5f61b2d82758c28ed
chuxin <15115359055@139.com>
2024-08-22 16:45:28 +0800

    feat: 支持打包进度条

--- node/demo-rollup/rollup.config.js
+++ node/demo-rollup/rollup.config.js
@@ -1,9 +1,21 @@
 import json from "@rollup/plugin-json";
 import babel from "@rollup/plugin-babel";
+import alias from "@rollup/plugin-alias";
 import terser from "@rollup/plugin-terser";
+import progress from "rollup-plugin-progress";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+
+// 废弃的包如下：
+// 1.rollup-plugin-babel   ==> @rollup/plugin-babel
+// 2.rollup-plugin-terser  ==> @rollup/plugin-terser
+
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
   input: "./src/main.ts",
@@ -39,12 +51,26 @@ export default {
     // 支持这样导入 import {name} from "../package.json"
     json(),
 
+    // 别名 vue 和 react 项目现在已经用烂了
+    alias({
+      entries: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    }),
+
     // 语法降级
     babel({
       // 编译库使用
       babelHelpers: "runtime",
       // 只转换源代码，不转换外部依赖
       exclude: "node_modules/**",
+    }),
+
+    // 进度条
+    progress({
+      clearLine: true, // 清除上一行的输出
+      completeChar: "=", // 完成部分的字符
+      incompleteChar: "-", // 未完成部分的字符
+      width: 50, // 进度条宽度
+      // 其他可选配置项
     }),
   ],
 };
