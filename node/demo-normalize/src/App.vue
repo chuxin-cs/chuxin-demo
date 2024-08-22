d56817cf07f52e2fb3ede61aff52f1aff45a51ba
chuxin <15115359055@139.com>
2024-08-22 13:38:32 +0800

    feat: demo-normalize用于对 css 样式重置的研究~

---
+++ node/demo-normalize/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Vite + normalize</h1>
+  </div>
+</template>
---
+++ node/demo-normalize/src/main.ts
@@ -0,0 +1,7 @@
+import App from "./App.vue";
+import { createApp } from "vue";
+
+// css
+import "normalize.css/normalize.css";
+
+createApp(App).mount("#app");
