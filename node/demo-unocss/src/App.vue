fa955b6fd541de7b305ceff68300bef0199a5ed2
chuxin <15115359055@139.com>
2024-08-22 11:39:18 +0800

    feat: rem to px

--- node/demo-unocss/src/App.vue
+++ node/demo-unocss/src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div>
-    <h1>Vite + Unocss</h1>
+    <h1 class="m-100 p-l-1">Vite + Unocss</h1>
+    <h1 m-100>Vite + Unocss</h1>
   </div>
 </template>
--- node/demo-unocss/src/main.ts
+++ node/demo-unocss/src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 // 引入css
-import "./styles/index.scss";
-import "@unocss/reset/normalize.css";
 import "virtual:uno.css";
 
 createApp(App).mount("#app");
