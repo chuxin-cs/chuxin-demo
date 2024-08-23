a142c4ea22e91be33561ee9d43cb71746ca93012
chuxin <15115359055@139.com>
2024-08-23 16:30:36 +0800

    feat: 使用 @babel/plugin-transform-arrow-functions 转换箭头函数

---
+++ node/demo-babel/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _index = require("./why-babel/index.js");
+console.log("chuxin: Babel...");
+
+// 01
+
+(0, _index.whyBabel)();
--- node/demo-babel/lib/why-babel/index.js
+++ node/demo-babel/lib/why-babel/index.js
@@ -1,12 +1,18 @@
-export const whyBabel = () => {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.whyBabel = void 0;
+const whyBabel = () => {
   // Babel 接收到的输入是： ES2015 箭头函数
-  let babelBefore = [1, 2, 3].map((n) => n + 1);
+  let babelBefore = [1, 2, 3].map(n => n + 1);
 
   // Babel 输出： ES5 语法实现的同等功能
   let babelAfter = [1, 2, 3].map(function (n) {
     return n + 1;
   });
-
   console.log("babel 之前：", babelBefore);
   console.log("babel 之后：", babelAfter);
 };
+exports.whyBabel = whyBabel;
