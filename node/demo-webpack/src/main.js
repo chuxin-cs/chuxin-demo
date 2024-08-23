77ea2363467456da572acf4df35218aa22e22025
chuxin <15115359055@139.com>
2024-08-23 13:46:46 +0800

    feat: 修改package.json描述~

--- node/demo-webpack/src/main.js
+++ node/demo-webpack/src/main.js
@@ -1,1 +1,18 @@
-console.log("hello world webpack!!!")
+console.log("hello world webpack!!!");
+
+export const print = () => {
+  return new Promise((resolve) => {
+    resolve(1);
+  });
+};
+
+export const Vue = () => {
+  console.log("Vue...");
+  print().then((res) => {
+    console.log(res, "print");
+  });
+};
+
+export default {
+  Vue,
+};
--- node/demo-webpack/webpack.config.js
+++ node/demo-webpack/webpack.config.js
@@ -3,11 +3,9 @@ const path = require("path");
 module.exports = {
     // 开发模式
     mode: "development",
-
     // 入口
     entry: path.resolve(__dirname, "./src/main.js"),
-
-    // 出口
+    // 输出
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "[name].js"
