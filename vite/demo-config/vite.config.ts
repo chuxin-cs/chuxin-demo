21263ebda974386d7ddef55c402e5e9aa77bbfab
chuxin <15115359055@139.com>
2024-08-26 10:24:23 +0800

    feat: 完成 build.rollupOptions 配置

--- vite/demo-config/vite.config.ts
+++ vite/demo-config/vite.config.ts
@@ -15,4 +15,22 @@ export default defineConfig({
     // vue plugin
     vue(),
   ],
+
+  // vite 打包相关配置
+  build: {
+    // vite 默认打包的文件是 500 KB 如果超过这个阈值就会报错 当前为了演示调整为 900 了
+    chunkSizeWarningLimit: 900,
+    // rollup 打包的相关配置 vite生产用的rollup打包
+    rollupOptions: {
+      output: {
+        // 1、manualChunks 默认方式
+        manualChunks: {
+          vendor: ["vue"],
+          "element-plus": ["element-plus"],
+          pinia: ["pinia"],
+          "vue-router": ["vue-router"],
+        },
+      },
+    },
+  },
 });
