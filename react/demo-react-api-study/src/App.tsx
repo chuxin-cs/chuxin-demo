1a826b9e074cd85f5b5f46dfe6aa5af0552e2021
chuxin <15115359055@139.com>
2024-10-09 09:05:29 +0800

    feat: 研究react slot插槽应用~

--- react/demo-react-api-study/src/App.tsx
+++ react/demo-react-api-study/src/App.tsx
@@ -1,11 +1,14 @@
+import PropsPages from "./pages/props";
 import UseStatePages from "./pages/useState";
 
 function App() {
   return (
     <>
       <UseStatePages />
+      <PropsPages />
     </>
   );
 }
 
+console.log(App, "app");
 export default App;
