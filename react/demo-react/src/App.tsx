7d44cbe75db46505525c421d4503de318460e3b0
chuxin <15115359055@139.com>
2024-10-25 16:01:50 +0800

    feat: 新增 prettier 对代码进行格式化~

--- react/demo-react/src/App.tsx
+++ react/demo-react/src/App.tsx
@@ -1,22 +1,20 @@
-import Router from "@/router";
-import {App as AntdApp} from 'antd';
-import {Helmet} from 'react-helmet-async';
+import Router from '@/router';
+import { App as AntdApp } from 'antd';
+import { Helmet } from 'react-helmet-async';
 import Logo from '@/assets/images/logo.png';
 
 function App() {
-    return (
-        <>
-            <AntdApp>
-
-                <Helmet>
-                    <title>demo react</title>
-                    <link rel="icon" href={Logo}/>
-                </Helmet>
-
-                <Router/>
-            </AntdApp>
-        </>
-    )
+  return (
+    <>
+      <AntdApp>
+        <Helmet>
+          <title>demo react</title>
+          <link rel="icon" href={Logo} />
+        </Helmet>
+        <Router />
+      </AntdApp>
+    </>
+  );
 }
 
-export default App
+export default App;
