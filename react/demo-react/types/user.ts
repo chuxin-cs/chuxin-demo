eb3e7b1540fb1f59bbe279cb912287ed9a3ab9d3
chuxin <15115359055@139.com>
2024-10-25 13:48:15 +0800

    feat: 基于 store/userStore 集成 zustand ~

---
+++ react/demo-react/types/user.ts
@@ -0,0 +1,45 @@
+import {BasicStatus, PermissionType} from './enum';
+
+export interface UserToken {
+    accessToken?: string;
+    refreshToken?: string;
+}
+
+export interface UserInfo {
+    id: string;
+    email: string;
+    username: string;
+    password?: string;
+    avatar?: string;
+    role?: Role;
+    status?: BasicStatus;
+    permissions?: Permission[];
+}
+
+export interface Role {
+    id: string;
+    name: string;
+    label: string;
+    status: BasicStatus;
+    order?: number;
+    desc?: string;
+    permission?: Permission[];
+}
+
+export interface Permission {
+    id: string;
+    parentId: string;
+    name: string;
+    label: string;
+    type: PermissionType;
+    route: string;
+    status?: BasicStatus;
+    order?: number;
+    icon?: string;
+    component?: string;
+    hide?: boolean;
+    hideTab?: boolean;
+    frameSrc?: string;
+    newFeature?: boolean;
+    children?: Permission[];
+}
