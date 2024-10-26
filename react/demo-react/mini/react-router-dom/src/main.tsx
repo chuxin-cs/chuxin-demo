7af15791151731b0af059017c9205a9045616f8d
chuxin <15115359055@139.com>
2024-10-26 13:46:00 +0800

    feat: 完成 mini react-router-dom

---
+++ react/demo-react/mini/react-router-dom/src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
+const root = createRoot(document.getElementById('root')! as HTMLElement);
+root.render(
+  <>
+    <App />
+  </>,
+);
