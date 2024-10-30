860f9f6e172fd55772d714ae84ed7396c709c7c7
chuxin <15115359055@139.com>
2024-10-30 09:17:02 +0800

    feat: 删除不必要代码~

--- react/packages/demo-react/src/App.jsx
+++ react/packages/demo-react/src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
+  return (<>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-       
       </div>
-    </>
-  )
+    </>);
 }
 
-export default App
+export default App;
